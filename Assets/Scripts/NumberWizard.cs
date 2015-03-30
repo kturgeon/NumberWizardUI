843cedbbfe7ab12f4e428400d0e2e86cf4f1b577
Keith Turgeon <keith.turgeon@live.com>
2015-03-30 10:58:20 -0400

    Initial Commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Text guessText;
+	
+	int max = 1000;
+	int min = 1;
+	int guess;
+		
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame () {
+		max = max + 1;
+		NextGuess();
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();		
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess();		
+	}
+	
+	public void GuessCorrect() {
+		StartGame();		
+	}
+	
+	void NextGuess () {
+		//guess = (max + min) / 2;
+		guess = Random.Range(min, max);
+		guessText.text = guess.ToString();
+		print ("Next guess is " + guess);
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Number Wizard (UI Edition)
+Third project in the Udemy course Complete Unity 5 Developer
